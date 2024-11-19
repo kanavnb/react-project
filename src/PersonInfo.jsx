23e3925b66f3c85a92cf9e230d1c0d80372ca1d8
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-19 22:13:26 +0600

    Show message when button is clicked

--- src/PersonInfo.jsx
+++ src/PersonInfo.jsx
@@ -1,17 +1,27 @@
 import './App.css';
 
+import { useState } from 'react';
+
 const PersonInfo = () => {
+    const [message, setMessage] = useState('');
+
+    const handleClick = () => {
+        setMessage('Message: Hello!');
+    };
+
     return (
         <div id="person-info" className="person-info">
             <h1>Name: Kanatbek Kamilov</h1>
             <p>Position: Developer</p>
-            <p>Company: Epam</p>
+            <p>Company: Spam</p>
             <p>City: Bishkek</p>
             <p>Age: 20</p>
             <p>Hobbies: Programming</p>
-            <button id="change-position-button">Click Me</button>
+            <button id="change-position-button" onClick={handleClick}>Click Me</button>
+            {message && <p>{message}</p>}
         </div>
     );
 };
 
 export default PersonInfo;
+
