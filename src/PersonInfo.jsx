dbf05c83c7926874556c52ea641eb8b8b0146caf
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-19 22:46:48 +0600

    Update position when button is clicked

--- src/PersonInfo.jsx
+++ src/PersonInfo.jsx
@@ -5,19 +5,24 @@ import { useState } from 'react';
 const PersonInfo = () => {
     const [message, setMessage] = useState('');
     const [buttonText, setButtonText] = useState('Click Me');
+    const [position, setPosition] = useState('Developer');
 
     const handleClick = () => {
         if (!message) {
             setMessage('Message: Hello!');
             setButtonText('Promote Position');
+        } else {
+            setPosition('Senior Developer');
         }
     };
+
+
 
     return (
         <div id="person-info" className="person-info">
             <h1>Name: Kanatbek Kamilov</h1>
-            <p>Position: Developer</p>
-            <p>Company: Spam</p>
+            <p>Position: {position}</p>
+            <p>Company: Epam</p>
             <p>City: Bishkek</p>
             <p>Age: 20</p>
             <p>Hobbies: Programming</p>
